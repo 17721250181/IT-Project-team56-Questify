c0e51ed4099ebfddf3dfc0d03bf287e0aa0d7a8c
Simon <zio1123581321@gmail.com>
2025-10-22 18:07:39 +1100

    updated logo path

--- frontend/src/components/common/Logo.jsx
+++ frontend/src/components/common/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => {
-    return <img src="../../../public/PlaceholderIcon.svg" alt="Logo" width={50} height={50} />;
+    return <img src="/PlaceholderIcon.svg" alt="Logo" width={50} height={50} />;
 };
 
 export default Logo;
