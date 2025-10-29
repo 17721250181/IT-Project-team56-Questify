9ee7429b39ac19e51d62dd192211c8110b440d45
Simon <zio1123581321@gmail.com>
2025-10-29 16:48:19 +1100

    Corrected migration orders preventing migration

--- backend/questions/migrations/0007_savedquestion.py
+++ backend/questions/migrations/0007_savedquestion.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("questions", "0005_update_num_attempts"),
+        ("questions", "0006_question_admin_feedback"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
